081ec2514910aeddb41112dcc55c5e2aa9930e6f
mariotmc <mariotomicx@gmail.com>
2023-07-23 14:44:03 +0200

    add yt

--- go.mod
+++ go.mod
@@ -2,11 +2,25 @@ module discord-bot
 
 go 1.20
 
-require github.com/bwmarrin/discordgo v0.27.1
+require (
+	github.com/Andreychik32/ytdl v1.0.4
+	github.com/bwmarrin/dgvoice v0.0.0-20210225172318-caaac756e02e
+	github.com/bwmarrin/discordgo v0.27.1
+)
 
 require (
-	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
-	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	github.com/antchfx/jsonquery v1.3.2 // indirect
+	github.com/antchfx/xpath v1.2.4 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/rs/zerolog v1.29.1 // indirect
+	layeh.com/gopus v0.0.0-20210501142526-1ee02d434e32 // indirect
+)
+
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.11.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
+	"github.com/Andreychik32/ytdl"
+	"github.com/bwmarrin/dgvoice"
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
@@ -19,25 +24,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		guildID := m.GuildID
-
-		if m.Author.ID == s.State.User.ID {
-			return
-		}
-
-		if m.Content == "hi" {
-			s.ChannelMessageSend(m.ChannelID, "hello")
-			voiceState, err := s.State.VoiceState(guildID, m.Author.ID)
-
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			authorChannelID := voiceState.ChannelID
-			s.ChannelVoiceJoin(guildID, authorChannelID, false, false)
-		}
-	})
+	sess.AddHandler(onMessageCreate)
 
 	sess.Identify.Intents = discordgo.IntentsAll
 
@@ -64,4 +51,117 @@ func loadToken() string {
 	token := os.Getenv("TOKEN")
 
 	return token
+}
+
+func onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate)  {
+		if m.Author.ID == s.State.User.ID || m.Author.Bot {
+			return
+		}
+
+		// Check if the message starts with the command prefix, such as "!play".
+		if !strings.HasPrefix(m.Content, "!play") {
+			return
+		}
+
+		// Extract the YouTube URL from the message content.
+		youtubeURL := strings.TrimSpace(strings.TrimPrefix(m.Content, "!play"))
+
+		// Play the song from the YouTube URL.
+		err := playFromYouTubeURL(s, m.GuildID, m.Author.ID, youtubeURL)
+		if err != nil {
+			fmt.Println("Error playing song: ", err)
+			return
+		}
+
+		if m.Content == "hi" {
+			s.ChannelMessageSend(m.ChannelID, "hello")
+
+			connectToVoiceChannel(s, m.GuildID, m.Author.ID)
+	}
+}
+
+func connectToVoiceChannel(s *discordgo.Session, guildID, userID string) (*discordgo.VoiceConnection, error) {
+	// Find the user in the guild's voice state.
+	vs, err := s.State.VoiceState(guildID, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	// Connect to the voice channel.
+	vc, err := s.ChannelVoiceJoin(guildID, vs.ChannelID, false, false)
+	if err != nil {
+		return nil, err
+	}
+
+	return vc, nil
+}
+
+func playFromYouTubeURL(s *discordgo.Session, guildID, userID, url string) error {
+	// Connect to the user's voice channel.
+	vc, err := connectToVoiceChannel(s, guildID, userID)
+	if err != nil {
+		return err
+	}
+	defer vc.Disconnect()
+
+	// Download the YouTube audio from the given URL.
+	audioFile, err := downloadYouTubeAudio(url)
+	if err != nil {
+		return err
+	}
+	defer os.Remove(audioFile)
+
+	// Start playing the audio in the voice channel.
+	err = playAudio(vc, audioFile)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func downloadYouTubeAudio(url string) (string, error) {
+	// Create a new video info object.
+	ctx := context.Background()
+	client := ytdl.DefaultClient
+	videoInfo, err := ytdl.GetVideoInfo(ctx, "https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+	if err != nil {
+		return "", err
+	}
+
+	// Download the audio stream.
+	audioFile, err := os.Create(videoInfo.Title + ".mp4")
+	if err != nil {
+		return "", err
+	}
+	defer audioFile.Close()
+
+	err = client.Download(ctx, videoInfo, videoInfo.Formats[0], audioFile)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v", err)
+
+	return videoInfo.Title + ".mp4", err
+}
+
+func playAudio(vc *discordgo.VoiceConnection, audioFile string) error {
+	// Open the audio file.
+	file, err := os.Open(audioFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Start speaking.
+	vc.Speaking(true)
+
+	// Send the audio packets.
+	dgvoice.PlayAudioFile(vc, "test.mp3", make(chan bool))
+
+	// Stop speaking.
+	vc.Speaking(false)
+
+	return nil
 }
